bfe73abf65206c6061503f90ae780244af36524d
tombjorn <tombjorn.audio@gmail.com>
2023-03-21 09:43:27 +0000

    initial process script commit

---
+++ src/lambdas/process.py
@@ -0,0 +1,22 @@
+# injest json
+# process into parquet
+# upload to s3 / write to local
+def load():
+    pass
+
+def process():
+    pass
+
+def upload_to_s3():
+    pass
+
+def save_to_local():
+    pass
+
+
+def main():
+    print('hello world')
+
+
+if __name__ == "__main__":
+    main()
