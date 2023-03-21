9f78efa645d817411acbb8b4bc2e9a6d3904f906
g-vernon <github@gvernon.com>
2023-03-21 12:20:27 +0000

    adds process function, tests, and print utility func

--- src/lambdas/process.py
+++ src/lambdas/process.py
@@ -13,19 +13,34 @@ def load_file_from_local(filepath):
     json_data.close()
     return data
 
-def process():
-    pass
+def process(table):
+    '''
+    Input: table, dict
+    Returns: table, pd data frame
+    '''
+    try:
+        df = pd.DataFrame(table['Data'], columns=table['Headers'])
+    except KeyError as e:
+        raise(e)
+    return df
 
 def upload_to_s3():
     pass
 
 def save_to_local():
+    '''
+    Input: table, pd data frame
+    Return: some kind of json object with a status code
+    '''
     pass
 
 def main():
     table1 = 'test/json_files/process_test_1.json'
     table1_data = load_file_from_local(table1)
     print(table1_data)
+    
+def print_csv(df, filepath):
+    df.to_csv(filepath)
 
 
 
--- test/lambdas/test_process.py
+++ test/lambdas/test_process.py
@@ -1,4 +1,7 @@
-from src.lambdas.process import (load_file_from_local)
+from src.lambdas.process import (load_file_from_local, process, print_csv)
+from pandas import DataFrame
+import pytest
+import numpy as np
 full_json = 'test/json_files/process_test_1.json'
 empty_json = 'test/json_files/process_test_2.json'
 no_data_json = 'test/json_files/process_test_3.json'
@@ -20,3 +23,46 @@ def test_load_file_from_local_dict_contains_data():
     ["foo2", "bar2", "foobar2"],
     ["foo3", "bar3", "foobar3"]
     ]
+    
+def test_process_with_valid_empty_data_from_local_returns_pd_dataframe():
+    raw_data = load_file_from_local(no_data_json)
+    dataframe = process(raw_data)
+    assert dataframe.empty
+
+def test_process_with_valid_data_from_local_returns_pd_dataframe():
+    raw_data = load_file_from_local(full_json)
+    dataframe = process(raw_data)
+    assert isinstance(dataframe, DataFrame)
+    
+def test_process_with_empty_json_throws_error():
+    raw_data = load_file_from_local(empty_json)
+    with pytest.raises(KeyError):
+        dataframe = process(raw_data)
+        assert True
+        
+def test_process_returns_dataframe_with_correct_columns():
+    raw_data = load_file_from_local(full_json)
+    dataframe = process(raw_data)
+    dataframe_cols = dataframe.columns.values
+    assert np.equal(dataframe_cols, ['Name', 'Type', 'Description']).all()
+
+def test_process_returns_dataframe_containing_input_data():
+    raw_data = load_file_from_local(full_json)
+    dataframe = process(raw_data)
+    headers = ['Name', 'Type', 'Description']
+    j = 0
+    #could refactor this to use enumerate
+    for header in headers:
+        for i in range(len(raw_data['Data'])):
+            assert dataframe[header][i] == raw_data['Data'][i][j]
+        j += 1
+    
+def test_print_pd_dataframe_to_csv():
+    raw_data = load_file_from_local(full_json)
+    dataframe = process(raw_data)
+    headers = ['Name', 'Type', 'Description']
+    j = 0
+    #could refactor this to use enumerate
+    print_csv(dataframe, 'test/csv_files/temp_test_1.csv')
+    # print(dataframe.head())
+    assert True
