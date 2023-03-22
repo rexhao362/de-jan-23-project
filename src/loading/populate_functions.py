41d942460bd94d4a44b75c10bc0ee5f5084fc478
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-22 10:16:34 +0000

    add tested populate_dim_design

--- src/loading/populate_functions.py
+++ src/loading/populate_functions.py
@@ -1,2 +1,3 @@
 from src.loading.populate_dim_currency import populate_dim_currency
 from src.loading.populate_dim_staff import populate_dim_staff
+from src.loading.populate_dim_design import populate_dim_design
