2afe42023c9e449b5d266184d8879bf272c7f572
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-22 16:20:36 +0000

    refactor using list comprehension

--- src/loading/format_dim_currency_data.py
+++ src/loading/format_dim_currency_data.py
@@ -11,7 +11,6 @@ def format_dim_currency_data(parquet_file_name, column_names=default_column_name
     Returns: a list of lists (each represent values of the table's row)
     Raises exception in case of error when reading parquet file, missing columns
     """
-    output_data = []
     
     try:
         data_frame = pandas.read_parquet(parquet_file_name)
@@ -21,13 +20,10 @@ def format_dim_currency_data(parquet_file_name, column_names=default_column_name
             if not column_name in data_frame.columns:
                 raise Exception( f'no "{column_name}" label found in "{parquet_file_name}"' )
 
-        for index, row in data_frame.iterrows():
-            output_data.append( \
-                [ row[column_name] for column_name in column_names ] \
-            )
+        return [
+            [ row[column_name] for column_name in column_names ]
+                for index, row in data_frame.iterrows()
+        ]
 
     except Exception as e:
         raise Exception( str(e) )
-
-    return output_data
-
