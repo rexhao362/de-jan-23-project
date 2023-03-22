9e410e1d77a79057e7065bd549e3e7951b45b878
tombjorn <tombjorn.audio@gmail.com>
2023-03-22 11:04:19 +0000

    adds test_build_dim_staff

---
+++ test/lambdas/process_functions/test_build_dim_staff.py
@@ -0,0 +1,29 @@
+from src.lambdas.process import (load_file_from_local, process, build_dim_staff)
+from pandas import DataFrame
+from numpy import (equal, int64)
+
+department_file = 'test/json_files/department_test_1.json'
+staff_file = 'test/json_files/staff_test_1.json'
+department_data = load_file_from_local(department_file)
+staff_data = load_file_from_local(staff_file)
+department_dataframe = process(department_data)
+staff_dataframe = process(staff_data)
+
+dim_staff = build_dim_staff(staff_dataframe, department_dataframe)
+
+def test_returns_dataframe():
+    assert isinstance(dim_staff, DataFrame)
+
+def test_returned_dataframe_has_expected_columns():
+    dim_staff_cols = dim_staff.columns.values
+    expected_cols = ['staff_id', 'first_name', 'last_name', 'department_name', 'location', 'email_address']
+    assert equal(dim_staff_cols, expected_cols).all()
+
+def test_returned_data_is_of_correct_datatype():
+    for i in range(dim_staff.shape[0]):
+        assert type(dim_staff['staff_id'][i]) == int64
+        assert type(dim_staff['first_name'][i]) == str
+        assert type(dim_staff['last_name'][i]) == str
+        assert type(dim_staff['department_name'][i]) == str
+        assert type(dim_staff['location'][i]) == str
+        assert type(dim_staff['email_address'][i]) == str
