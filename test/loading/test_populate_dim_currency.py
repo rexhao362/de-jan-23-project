e9df5951808c5bdcdb17fe27d16b717b5db5901d
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-22 09:44:36 +0000

    use _test_populate_independent_table to run tests

--- test/loading/test_populate_dim_currency.py
+++ test/loading/test_populate_dim_currency.py
@@ -1,50 +1,21 @@
-from pg8000.native import Connection
-from src.loading.populate_dim_currency import populate_dim_currency
-from src.loading.config import (user, passwd, test_db)
+from src.util import _test_populate_independent_table
+
+table_name = "dim_currency"
 
 def test_loads_one_row_when_passed_one_currency_item():
-    with Connection(user, password=passwd, database=test_db) as connection:
-        # arrange
-        input_currency_data = [
-            [1, "USD", "US dollar"]
-        ]
-        
-        # act
-        connection.run('DELETE FROM dim_currency')
-        populate_dim_currency(connection, input_currency_data)
-        rows = connection.run('SELECT * FROM dim_currency')
+    # arrange
+    input_currency_data = [
+        [1, "USD", "US dollar"]
+    ]
 
-        # assert
-        assert len(rows) == 1
-        row = rows[0]
-        columns = len(row)
-        assert columns == 3
-
-        for index in range(columns):
-            assert row[index] == input_currency_data[0][index]
+    _test_populate_independent_table(table_name, input_currency_data)
 
 def test_loads_multiple_rows_when_passed_multiple_currency_item():
-    with Connection(user, password=passwd, database=test_db) as connection:
-        # arrange
-        input_currency_data = [
-            [1, "USD", "US dollar"],
-            [5, "GBP", "pound"],
-            [13, "JPY", "yen"]
-        ]
-        
-        num_columns = len(input_currency_data[0])
-        num_rows = len(input_currency_data)
-        
-        # act
-        connection.run('DELETE FROM dim_currency')
-        populate_dim_currency(connection, input_currency_data)
-        rows = connection.run('SELECT * FROM dim_currency')
+    # arrange
+    input_currency_data = [
+        [1, "USD", "US dollar"],
+        [5, "GBP", "pound"],
+        [13, "JPY", "yen"]
+    ]
 
-        # assert
-        assert len(rows) == num_rows
-        for row_index in range(num_rows):
-            row = rows[row_index]
-            assert len(row) == num_columns
-
-            for column_index in range(num_columns):
-                assert row[column_index] == input_currency_data[row_index][column_index]
+    _test_populate_independent_table(table_name, input_currency_data)
