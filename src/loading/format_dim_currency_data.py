40041b7ffe7555569b39b2dbd60b62769e6ec754
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-22 16:11:57 +0000

    add working format_dim_currency_data

--- src/loading/format_dim_currency_data.py
+++ src/loading/format_dim_currency_data.py
@@ -1,20 +1,33 @@
 import pandas
 
-column_names = [
+default_column_names = [
     "currency_id",
     "currency_code",
     "currency_name"
 ]
 
-def format_dim_currency_data(parquet_file_name):
+def format_dim_currency_data(parquet_file_name, column_names=default_column_names):
+    """
+    Returns: a list of lists (each represent values of the table's row)
+    Raises exception in case of error when reading parquet file, missing columns
+    """
     output_data = []
     
-    data_frame = pandas.read_parquet(parquet_file_name)
+    try:
+        data_frame = pandas.read_parquet(parquet_file_name)
 
-    for index, row in data_frame.iterrows():
-        output_data.append( \
-            [ row[column_name] for column_name in column_names ] \
-        )
+        # check that all columns necessary for the table are present
+        for column_name in column_names:
+            if not column_name in data_frame.columns:
+                raise Exception( f'no "{column_name}" label found in "{parquet_file_name}"' )
+
+        for index, row in data_frame.iterrows():
+            output_data.append( \
+                [ row[column_name] for column_name in column_names ] \
+            )
+
+    except Exception as e:
+        raise Exception( str(e) )
 
     return output_data
 
--- test/loading/test_format_dim_currency_data.py
+++ test/loading/test_format_dim_currency_data.py
@@ -1,4 +1,7 @@
+import pytest
 from src.loading.format_dim_currency_data import format_dim_currency_data
+
+# happy path
 
 def test_returns_empty_list_when_passed_table_with_no_data():
     input_file_name = "test/loading/input_files/empty_dim_currency.parquet"
@@ -20,6 +23,25 @@ def test_returns_multiple_elements_when_passed_table_with_multiple_rows():
     ]
     assert format_dim_currency_data(input_file_name) == ref_output
 
-# invalid format file
-# different set of columns
-# inalid data types
+# sad path
+
+def test_raises_exception_on_errors():
+    # file does not exist
+    with pytest.raises(Exception) as e:
+        format_dim_currency_data("")
+
+    # file of 0 bytes
+    with pytest.raises(Exception) as e:
+        format_dim_currency_data("test/loading/input_files/invalid.parquet")
+
+    # invalid format type
+    with pytest.raises(Exception) as e:
+        format_dim_currency_data("./README.md")
+    
+    # does not have all necessary columns
+    with pytest.raises(Exception) as e:
+        format_dim_currency_data("test/loading/input_files/dim_staff.parquet")
+
+    # inalid data types - MAYBE TODO?
+    # with pytest.raises(Exception) as e:
+    #     format_dim_currency_data("test/loading/input_files/dim_currency.parquet")
