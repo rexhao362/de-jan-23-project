802686ea91075f4fe71830ecd88768463c38ccec
Rexhao362 <56262178+Rexhao362@users.noreply.github.com>
2023-03-22 11:09:54 +0000

    completed test files

---
+++ ingestion_function/ingestion.py
@@ -0,0 +1,60 @@
+import os
+import json
+from decimal import Decimal
+from ingestion_function.connection import con
+from datetime import datetime
+
+# Create data dir
+if not os.path.exists('./ingestion-function/data'):
+    os.makedirs('./ingestion-function/data')
+
+
+# Queries database for table names
+def get_table_names():
+    table_names = con.run(
+        'SELECT table_name, table_schema FROM information_schema.tables')
+    return [item[0] for item in table_names if item[1] == 'public']
+
+
+# Extracts column names for current table
+def get_headers():
+    return [c['name'] for c in con.columns]
+
+
+# Extracts list of table data
+def get_table_data(table_name):
+    table_data = con.run(f'SELECT * FROM {table_name}')
+    table_data.insert(0, get_headers())
+    return table_data
+
+
+# Writes dictionary of table to file
+def data_ingestion():
+    for table_name in get_table_names():
+        table_data = get_table_data(table_name)
+        for row in table_data:
+            for i in range(len(row)):
+                if isinstance(row[i], datetime):
+                    row[i] = row[i].strftime('%Y-%m-%dT%H:%M:%S.%f')
+                elif isinstance(row[i], Decimal):
+                    row[i] = float(row[i])
+
+        dict = {
+            'table_name': table_name,
+            'headers': table_data[0],
+            'data': table_data[1:]
+        }
+
+        with open(f'./ingestion_function/data/{table_name}.json', 'w') as f:
+            f.write(json.dumps(dict))
+
+def last_updated_date():
+    pass
+
+
+def upload_to_s3():
+    pass
+
+
+def store_last_updated():
+    pass
