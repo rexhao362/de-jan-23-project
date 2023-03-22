545018f5269bc483ea01c1246953a1b0577425c7
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-22 10:15:43 +0000

    delete test code

--- src/loading/populate_dim_staff.py
+++ src/loading/populate_dim_staff.py
@@ -1,8 +1,6 @@
 def populate_dim_staff(connection, data, table_name="dim_staff"):
     values = ",".join( [ f"('{record[0]}', '{record[1]}', '{record[2]}', '{record[3]}', '{record[4]}', '{record[5]}')" for record in data] )
-    # record = data[0]
-    # values = f"('{record[0]}', '{record[1]}', '{record[2]}', '{record[3]}', '{record[4]}', '{record[5]}')"
-
+    
     res = connection.run( f"\
     INSERT INTO {table_name}\
     (staff_id, first_name, last_name, department_name, location, email_address)\
