863b1955fee5eee1a1bd85533d5632e5eaf05073
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-23 22:52:28 +0000

    use make_schema_table_name function

--- src/lambdas/load/utils/test_populate_tables.py
+++ src/lambdas/load/utils/test_populate_tables.py
@@ -1,12 +1,13 @@
 from pg8000.native import Connection
-import src.lambdas.load.populate_functions as populate_functions
 from src.environ.warehouse_db import env_warehouse_db as env
+import src.lambdas.load.populate_functions as populate_functions
+from src.utils.db.make_schema_table_name import make_schema_table_name
 
 def _test_populate_independent_table(table_name, input_data):
     with Connection(env["user"], password=env["password"], host=env["host"], port=env["port"], database=env["database"]) as connection:
         num_columns = len(input_data[0])
         num_rows = len(input_data)
-        full_table_name = f'{env["schema"]}.{table_name}'
+        full_table_name = make_schema_table_name( env["schema"], table_name)
         
         # act
         connection.run(f'DELETE FROM {full_table_name}')
