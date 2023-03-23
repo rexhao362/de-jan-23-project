2f53825ddb027c77d754844e1dc6aec6add184c4
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-23 16:11:57 +0000

    use env variables

--- src/loading/utils/test_populate_tables.py
+++ src/loading/utils/test_populate_tables.py
@@ -1,24 +1,17 @@
 from pg8000.native import Connection
 import src.loading.populate_functions as populate_functions
-from os import environ
+from src.environ.warehouse_db import env_warehouse_db as env
 
-user = environ.get('WAREHOUSE_DB_USER')
-password = environ.get('WAREHOUSE_DB_PASSWORD')
-host = environ.get('WAREHOUSE_DB_HOST', 'localhost')
-port = environ.get('WAREHOUSE_DB_PORT', 5432)
-database = environ.get('WAREHOUSE_DB_DATABASE')
-#schema = environ.get('WAREHOUSE_DB_DATABASE_SCHEMA')
-
-def _test_populate_independent_table(schema, table_name, input_data):
-    with Connection(user, password=password, host=host, port=port, database=database) as connection:
+def _test_populate_independent_table(table_name, input_data):
+    with Connection(env["user"], password=env["password"], host=env["host"], port=env["port"], database=env["database"]) as connection:
         num_columns = len(input_data[0])
         num_rows = len(input_data)
-        full_table_name = f'{schema}.{table_name}'
+        full_table_name = f'{env["schema"]}.{table_name}'
         
         # act
         connection.run(f'DELETE FROM {full_table_name}')
         populate_function = getattr(populate_functions, f'populate_{table_name}' )
-        populate_function(connection, input_data, schema, table_name)
+        populate_function(connection, input_data, env["schema"], table_name)
         rows = connection.run(f'SELECT * FROM {full_table_name}')
 
         # assert
