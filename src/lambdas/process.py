504fdcb71c504fd0e7b8c02374c76b0dae6323e0
tombjorn <tombjorn.audio@gmail.com>
2023-03-23 12:16:46 +0000

    adds utils and build module files, refactors our process.py script

--- src/lambdas/process.py
+++ src/lambdas/process.py
@@ -2,347 +2,44 @@ import pandas as pd
 import json
 import numpy as np
 import pyarrow
-
-#! INGESTION LABELS ARE LOWER CASE
-#! A LOT OF DATA HAS NULL VALUES SO DONT TEST FOR NONE
-
-def load_file_from_s3():
-    pass
-
-def load_file_from_local(filepath):
-    """
-    Input: filepath, string
-    Returns: data, dict
-    """
-    json_data = open(filepath)
-    data = json.load(json_data)
-    json_data.close()
-    return data
-
-def process(table):
-    '''
-    Input: table, dict
-    Returns: table, pd data frame
-    '''
-    try:
-        df = pd.DataFrame(table['data'], columns=table['headers'])
-    except KeyError as e:
-        raise(e)
-    return df
-
-def upload_to_s3():
-    pass
-
-def save_to_local():
-    '''
-    Input: table, pd data frame
-    Return: some kind of json object with a status code
-    '''
-    pass
-
-def print_csv(df, filepath):
-    df.to_csv(filepath)
-
-def build_dim_design(dataframe):
-
-    """
-    Input: design, dataframe
-    Returns: dim_design, dataframe
-
-    BUILD DIM_DESIGN
-
-    - strips some data from original design table
-
-    input columns: 
-    [design_id, created_at, last_updated, design_name, file_location, file_name]
-
-    output columns: 
-    [design_id, design_name, file_location, file_name]
-
-    """
-    df = dataframe.copy()
-    dim_design = df.drop(columns=['created_at', 'last_updated'])
-    dim_design = dim_design
-    return dim_design[['design_id', 'design_name', 'file_location', 'file_name']]
-
-
-def build_dim_currency(dataframe):
-
-    """
-    Input: currency, dataframe
-    Returns: dim_currency, dataframe
-
-    BUILD DIM_CURRENCY
-
-    - strips some data from original currency table
-    - inserts currency_name value depending on currency_code
-
-    input columns:
-    [currency_id, currency_code, created_at, last_updated]
-
-    output columns:
-    [currency_id, currency_code, currency_name]
-
-    """
-    df = dataframe.copy()
-    dim_currency = df.drop(columns=['created_at', 'last_updated'])
-
-    # {"GBP" : "Pounds", "USD": "Dollars", "EUR": Euros}
-
-    currency_names = []
-    for item in dim_currency['currency_code']:
-        if item == "GBP":
-            currency_names.append('Pounds')
-        elif item == "USD":
-            currency_names.append('Dollars')
-        elif item == "EUR":
-            currency_names.append('Euros')
-        else:
-            currency_names.append(None)
-
-    dim_currency['currency_name'] = currency_names
-    dim_currency = dim_currency[['currency_id','currency_code', 'currency_name']]
-    return dim_currency
-
-def build_fact_sales_order(sales_order_dataframe):
-
-    """
-    Input: dataframes, dict
-    Returns: fact_sales_order, dataframe
-    
-    BUILD FACT_SALES_ORDER
-
-    Consider using a dict containing ref memory locations of DataFrames,
-    or keyword arguments or default arguments?
-
-    headers: ["sales_order_id", "created_at", "last_updated", "design_id", "staff_id", "counterparty_id", "units_sold", "unit_price", "currency_id", "agreed_delivery_date", "agreed_payment_date", "agreed_delivery_location_id"]
-    """
-    sales_order = sales_order_dataframe.copy()
-
-    # convert timestamps to datetime objects for 'last_updated' and 'created_at'
-    sales_order['last_updated'] = pd.to_datetime(sales_order['last_updated'])
-    sales_order['created_at'] = pd.to_datetime(sales_order['created_at'])
-
-    # can now generate 'last_updated_time' and 'created_time'
-    # sales_order['last_updated_time'] = sales_order['last_updated'].dt.strftime('%H:%M:%S')
-    sales_order['last_updated_time'] = sales_order['last_updated'].dt.time
-    sales_order['created_time'] = sales_order['created_at'].dt.time
-
-    # trunkate dates
-    sales_order['last_updated'] = sales_order['last_updated'].dt.date
-    sales_order['created_at'] = sales_order['created_at'].dt.date
-
-    #generate rest of date columns
-    columns = ['agreed_delivery_date', 'agreed_payment_date']
-    for col in columns:
-        sales_order = timestamp_to_date(sales_order, col)
-    
-    # rename columns
-    sales_order = sales_order.rename(columns={"created_at":"created_date", "last_updated":"last_updated_date", "staff_id":"sales_staff_id"})
-
-    order_list = ['sales_order_id', 'created_date', 'created_time', 'last_updated_date', 'last_updated_time', 'sales_staff_id', 'counterparty_id', 'units_sold', 'unit_price', 'currency_id', 'design_id', 'agreed_payment_date', 'agreed_delivery_date', 'agreed_delivery_location_id']
-
-    sales_order = sales_order[order_list]
-    return sales_order
-
-
-def timestamp_to_date(table, column):
-    """
-    Input[0]: table, dataframe 
-    Input[1]: column, string
-
-    Returns: table, dataframe
-    """
-    table[column] = pd.to_datetime(table[column])
-    table[column] = table[column].dt.date
-    return table
-
-
-def build_dim_staff(staff_dataframe, department_dataframe):
-    """
-    BUILD DIM_STAFF
-    Input[0]: staff, DataFrame
-    Input[1]: department, DataFrame
-    Returns: dim_staff, DataFrame
-    """
-    dim_staff = staff_dataframe.copy()
-    department_id = dim_staff['department_id']
-    dim_staff = dim_staff.drop(columns=['department_id', 'created_at', 'last_updated'])
-    length = dim_staff.shape[0]
-
-    department_name_col = np.empty(length, dtype='U15')
-    location_col = np.empty(length, dtype='U10')
-
-    for i, id in enumerate(department_id):
-        department = department_dataframe.query(f"department_id == {id}")
-        department_name_col[i] = department['department_name'].item()
-        location_col[i] = department['location'].item()
-    
-    dim_staff['department_name'] = department_name_col
-    dim_staff['location'] = location_col
-    dim_staff = dim_staff[['staff_id', 'first_name', 'last_name', 'department_name', 'location', 'email_address']]
-    return dim_staff
-
-
-def build_dim_location(dataframe):
-    """
-    Input: addresses, dataframe
-    Returns: dim_location, dataframe
-
-    BUILD DIM_LOCATION
-    input columns:
-    [address_id, address_line_1, address_line_2, district, city, postal_code, country, phone, created_at, last_updated]
-
-    output columns:
-    [
-    location_id, address_line_1, address_line_2, district, city, postal_code, country, phone
-    ]
-    """
-    df = dataframe.copy()
-    dim_location = df.drop(columns=['created_at', 'last_updated'])
-    dim_location = dim_location.rename(columns={'address_id':'location_id'})
-    return dim_location
-
-def build_dim_date(start_date, end_date):
-    """
-    BUILD DIM_DATE
-    arg1 (string): first date (inclusive) in the range to be generated, format yyyy/mm/dd
-    arg2 (string): last date (inclusive) in the range to be generated, format yyyy/mm/dd
-    Returns: Pandas dataframe with date_id index, and columns for year, month, day, day_of_week, day_name, month_name, and quarter.
-    
-    - Generates and populates a dataframe with a range of dates from start_date to end_date
-    """
-    #generate a date time index
-    dti = pd.date_range(start=start_date, end=end_date).to_series()
-    # df = dti.to_frame(index=False)
-    years = dti.dt.year
-    months = dti.dt.month
-    days = dti.dt.day
-    day_of_week = dti.dt.day_of_week
-    day_name = dti.dt.day_name()
-    month_name=dti.dt.month_name()
-    quarter = dti.dt.quarter
-    d={
-        'date_id': dti,
-        'year': years,
-        'month': months,
-        'day': days,
-        'day_of_week': day_of_week,
-        'day_name': day_name,
-        'month_name': month_name,
-        'quarter': quarter
-    }
-    df = pd.DataFrame(data=d)
-    return df
-    
-
-def build_dim_counterparty(original_dataframe, address_dataframe):
-    """
-    BUILD DIM_COUNTERPARTY
-    arg1: original counterparty dataframe
-    arg2: original address dataframe
-    
-    input columns = [counterparty_id, counterparty_legal_name, legal_address_id, commercial_contact, delivery_contact, created_at, last_updated]
-     1
-    output columns = [counterparty_id, counterparty_legal_name, counterparty_legal_address_line_1, counterparty_legal_address_line_2, counterparty_legal_district, counterparty_legal_city, counterparty_legal_postal_code, counterparty_legal_country, counterparty_legal_phone_number]
-    """
-    df = original_dataframe.copy()
-    ids = original_dataframe['legal_address_id']
-    print(ids)
-    df = df.drop(columns=['last_updated', 'created_at', 'delivery_contact', 'commercial_contact', 'legal_address_id'])
-    #print(ids)
-    length = df.shape[0]
-    #init new cols
-    l_add_1 = np.empty(length, dtype='U10')
-    l_add_2 = np.empty(length, dtype='U10')
-    l_district = np.empty(length, dtype='U10')
-    l_country = np.empty(length, dtype='U10')
-    l_city = np.empty(length, dtype='U10')
-    l_postal_code = np.empty(length, dtype='U10')
-    l_phone = np.empty(length, dtype='U10')
-    #for each record in dataframe and corresponding address_id
-    for i, id in enumerate(ids):
-    
-        #query the address table for the right id
-        address = address_dataframe.query(f"address_id == {id}")
-
-        #initialize the columns
-        # print(address['address_line_1'].item())
-        l_add_1[i] = address['address_line_1'].item()
-        l_add_2[i] = address['address_line_2'].item()
-        l_district[i] = address['district'].item()
-        l_city[i] = address['city'].item()
-        l_country[i] = address['country'].item()
-        l_postal_code[i] = address['postal_code'].item()
-        l_phone[i] = address['phone'].item()
-        
-    df['counterparty_legal_address_line_1'] = l_add_1
-    df['counterparty_legal_address_line_2'] = l_add_2
-    df['counterparty_legal_district'] = l_district
-    df['counterparty_legal_city'] = l_city
-    df['counterparty_legal_postal_code'] = l_postal_code
-    df['counterparty_legal_country'] = l_country
-    df['counterparty_legal_phone_number'] = l_phone
-        
-    return df
-    
-    
-    # use ids to fetch from address:
-    # counterparty_legal_address_line_1
-    # counterparty_legal_address_line_2
-    # counterparty_legal_district
-    # counterparty_legal_city
-    # counterparty_legal_postcode
-    # counterparty_legal_county
-
-def generate_local_parquet(table):
-    """
-    Input: table, dataframe
-    returns: table, parquet
-    """
-    # get table name
-    # use in filepath?
+from utils import (load_file_from_local, process)
 
 def main():
-    # BUILD CURRENCY DATAFRAME
+    # LOAD CURRENCY DATAFRAME
     curr_path = 'test/json_files/currency_test_2.json'
     curr_data = load_file_from_local(curr_path)
     curr_dataframe = process(curr_data)
 
-    # BUILD STAFF DATAFRAME
+    # LOAD STAFF DATAFRAME
     staff_path = 'test/json_files/staff_test_1.json'
     staff_data = load_file_from_local(staff_path)
     staff_dataframe = process(staff_data)
 
-    # BUILD DEPARTMENT DATAFRAME
+    # LOAD DEPARTMENT DATAFRAME
     dep_path = 'test/json_files/department_test_1.json'
     dep_data = load_file_from_local(dep_path)
     department_dataframe = process(dep_data)
 
-    # BUILD ADDRESS DATAFRAME
+    # LOAD ADDRESS DATAFRAME
     address_path = 'test/json_files/address_test_2.json'
     address_data = load_file_from_local(address_path)
     address_dataframe = process(address_data)
 
-    # BUILD COUNTERPARTY DATAFRAME
+    # LOAD COUNTERPARTY DATAFRAME
     counter_path = 'test/json_files/counterparty_test_2.json'
     counter_data = load_file_from_local(counter_path)
     counter_dataframe = process(counter_data)
 
-    # BUILD DESIGN DATAFRAME
+    # LOAD DESIGN DATAFRAME
     design_path = 'test/json_files/design_test_2.json'
     design_data = load_file_from_local(design_path)
     design_dataframe = process(design_data)
 
-    # BUILD SALES DATAFRAME
+    # LOAD SALES DATAFRAME
     sales_path = 'test/json_files/sales_order_test_1.json'
     sales_data = load_file_from_local(sales_path)
     sales_dataframe = process(sales_data)
     
-    # BUILD DATE DATAFRAME
-
-
-
 
 if __name__ == "__main__":
     main()
