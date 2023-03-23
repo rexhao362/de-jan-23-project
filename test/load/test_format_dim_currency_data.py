e7c15b7dd5d5b572d8a031b727b05d53dedf3e0f
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-23 22:09:39 +0000

    refactor tests

--- test/load/test_format_dim_currency_data.py
+++ test/load/test_format_dim_currency_data.py
@@ -3,33 +3,42 @@ import pandas
 from src.load.format_dim_currency_data import format_dim_currency_data
 
 # happy path
+test_path = "test/load/input_files"
+
+def _test_format_dim_currency_data(path, ref_output = None):
+    data_frame = pandas.read_parquet(path)
+    assert format_dim_currency_data(data_frame) == ref_output
 
 def test_returns_empty_list_when_passed_table_with_no_data():
-    input_file_name = "test/load/input_files/empty_dim_currency.parquet"
-    data_frame = pandas.read_parquet(input_file_name)
-    assert format_dim_currency_data(data_frame) == []
+    path = f'{test_path}/empty_dim_currency.parquet'
+    ref_output = []
+    _test_format_dim_currency_data(path, ref_output )
 
 def test_returns_one_element_when_passed_table_with_one_row():
-    input_file_name = "test/load/input_files/one_dim_currency.parquet"
-    data_frame = pandas.read_parquet(input_file_name)
+    path = f'{test_path}/one_dim_currency.parquet'
     ref_output = [
         [1, "GBP", "Pounds"]
     ]
-    assert format_dim_currency_data(data_frame) == ref_output
+    _test_format_dim_currency_data(path, ref_output )
 
 def test_returns_multiple_elements_when_passed_table_with_multiple_rows():
-    input_file_name = "test/load/input_files/dim_currency.parquet"
-    data_frame = pandas.read_parquet(input_file_name)
+    path = f'{test_path}/dim_currency.parquet'
     ref_output = [
         [1, "GBP", "Pounds"],
         [2, "USD", "Dollars"],
         [3, "EUR", "Euros"]
     ]
-    assert format_dim_currency_data(data_frame) == ref_output
+    _test_format_dim_currency_data(path, ref_output )
 
 # sad path
 
 def test_raises_exception_on_errors():
+    # does not have all necessary columns
+    with pytest.raises(Exception) as e:
+        path = f'{test_path}/dim_staff.parquet'
+        _test_format_dim_currency_data(path)
+
+    # this is part of the original read_parquet, no need to test
     # # file does not exist
     # with pytest.raises(Exception) as e:
     #     format_dim_currency_data("")
@@ -42,11 +51,7 @@ def test_raises_exception_on_errors():
     # with pytest.raises(Exception) as e:
     #     format_dim_currency_data("./README.md")
     
-    # does not have all necessary columns
-    with pytest.raises(Exception) as e:
-        input_file_name = "test/load/input_files/dim_staff.parquet"
-        data_frame = pandas.read_parquet(input_file_name)
-        format_dim_currency_data(data_frame)
+    
 
     # inalid data types - MAYBE TODO?
     # with pytest.raises(Exception) as e:
