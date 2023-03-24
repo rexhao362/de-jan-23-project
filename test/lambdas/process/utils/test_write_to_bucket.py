3eb78f9cf960775ac3d0a6e61898e40e7d34d611
tombjorn <tombjorn.audio@gmail.com>
2023-03-24 13:55:08 +0000

    adds test_write_to_file

--- test/lambdas/process/utils/test_write_to_bucket.py
+++ test/lambdas/process/utils/test_write_to_bucket.py
@@ -1,14 +1,16 @@
 import boto3
 from moto import mock_s3
-from moto.core import patch_client
 from pandas import read_parquet
 import pytest
 import os
-import logging
+import io
+import boto3
+import pyarrow.parquet as pq
+from numpy import equal
 
 dataframe1 = read_parquet('test/lambdas/process/parquets/dim_currency.parquet')
 dataframe2 = read_parquet('test/lambdas/process/parquets/dim_currency_formatted.parquet')
-bucket_name = "test_bucket"
+bucket_name = "test_bucket_1"
 
 
 @pytest.fixture(scope="module")
@@ -26,7 +28,7 @@ def s3(aws_credentials):
         yield boto3.client("s3", region_name="us-east-1")
 
 def test_create_bucket(s3):
-    s3.create_bucket(Bucket="test_bucket")
+    s3.create_bucket(Bucket=bucket_name)
     result = s3.list_buckets()
     assert len(result["Buckets"]) == 1
     assert result["Buckets"][0]["Name"] == bucket_name
@@ -46,7 +48,7 @@ def test_returns_status_code_200_with_successful_write(s3):
     from src.lambdas.process.utils import (write_to_bucket)
     obj_key_1 = 'test/test_1'
     upload = write_to_bucket(bucket_name, dataframe1, obj_key_1)
-    objects = s3.list_objects_v2(Bucket='test_bucket')
+    objects = s3.list_objects_v2(Bucket=bucket_name)
     assert obj_key_1 + ".parquet" in objects['Contents'][0]['Key']
     assert upload['status'] == 200
     
@@ -64,22 +66,22 @@ def test_key_is_maintained_in_bucket(s3):
     obj_key_2 = 'test/test_2'
     write_to_bucket(bucket_name, dataframe1, obj_key_1)
     write_to_bucket(bucket_name, dataframe1, obj_key_2)
-    objects = s3.list_objects_v2(Bucket='test_bucket')
+    objects = s3.list_objects_v2(Bucket=bucket_name)
     assert obj_key_1 + ".parquet" in objects['Contents'][0]['Key']
     assert obj_key_2 + ".parquet" in objects['Contents'][1]['Key']
 
-    
 def test_file_is_still_valid_once_written_to_bucket(s3):
-    pass
+    from src.lambdas.process.utils import (write_to_bucket)
+    buffer = io.BytesIO()
+    resource = boto3.resource('s3')
+    s3_object = resource.Object('test_bucket_1', 'test/test_1.parquet')
+    s3_object.download_fileobj(buffer)
+    table = pq.read_table(buffer)
+    df = table.to_pandas()
+    assert equal(df.columns.values,['currency_id', 'currency_code', 'currency_name']).all()
+    assert equal(df['currency_id'], dataframe1['currency_id']).all()
+    assert equal(df['currency_code'], dataframe1['currency_code']).all()
+    assert equal(df['currency_name'], dataframe1['currency_name']).all()
 
-    # download = load_file_from_s3(bucket_name,f"{obj_key}.parquet")
-    # print(download)
-
-    # result = s3.list_objects_v2(Bucket='test_bucket')
-    # def test_uploads_files(bucket, s3):
-#     result = s3.list_objects_v2(Bucket='test_bucket')
-#     for item in result['Contents']:
-#         print(item)
-    
 
 
