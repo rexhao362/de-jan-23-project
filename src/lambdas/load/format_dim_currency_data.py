81592f3b5134b5479c7d770734c3baa029f7862a
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-24 13:06:31 +0000

    fix function description

--- src/lambdas/load/format_dim_currency_data.py
+++ src/lambdas/load/format_dim_currency_data.py
@@ -7,7 +7,8 @@ default_column_names = [
 def format_dim_currency_data(data_frame, column_names=default_column_names):
     """
     Returns: a list of lists (each represent values of the table's row)
-    Raises exception in case of error when reading parquet file, missing columns
+    Raises
+        Exception in case of missing columns
     """
     # check that all columns necessary for the table are present
     for column_name in column_names:
