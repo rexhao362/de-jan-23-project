09c1916f2eec4d00f4cc7d5d8c7bfb71cc150f5d
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-28 21:54:34 +0100

    add dim_design

--- src/lambdas/load/db_schema.py
+++ src/lambdas/load/db_schema.py
@@ -2,26 +2,32 @@ from src.lambdas.load.utils.data_table import DataTable
 
 db_schema = [
     DataTable("dim_staff", {
-        "staff_id": "int",
-        "first_name": "varchar",
-        "last_name": "varchar",
-        "department_name": "varchar",
-        "location": "varchar",
-        "email_address": "varchar"}
-    ),
+        "staff_id": "INT",
+        "first_name": "VARCHAR",
+        "last_name": "VARCHAR",
+        "department_name": "VARCHAR",
+        "location": "VARCHAR",
+        "email_address": "VARCHAR"}
+        ),
     DataTable("dim_location", {
-        "location_id": "int",
-        "address_line_1": "varchar",
-        "address_line_2": "varchar",
-        "district": "varchar",
-        "city": "varchar",
-        "postal_code": "varchar",
-        "country": "varchar",
-        "phone": "varchar"}, dont_import = True
-    ),
+        "location_id": "INT",
+        "address_line_1": "VARCHAR",
+        "address_line_2": "VARCHAR",
+        "district": "VARCHAR",
+        "city": "VARCHAR",
+        "postal_code": "VARCHAR",
+        "country": "VARCHAR",
+        "phone": "VARCHAR"}, dont_import = True
+        ),
     DataTable("dim_currency", {
         "currency_id": "INT",
         "currency_code": "VARCHAR",
         "currency_name": "VARCHAR"}
-    ),
+        ),
+    DataTable("dim_design", {
+        "design_id": "INT",
+        "design_name": "VARCHAR",
+        "file_location": "VARCHAR",
+        "file_name": "VARCHAR"}
+        )
 ]
