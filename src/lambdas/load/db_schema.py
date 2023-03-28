9b17369456f9ad63469c54888f072d24000cd9fd
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-28 21:45:55 +0100

    add dim_location to schema (dont_import because of address_line_2)

--- src/lambdas/load/db_schema.py
+++ src/lambdas/load/db_schema.py
@@ -1,17 +1,27 @@
 from src.lambdas.load.utils.data_table import DataTable
 
 db_schema = [
-    DataTable("dim_currency", {
-        "currency_id": "INT",
-        "currency_code": "VARCHAR",
-        "currency_name": "VARCHAR"}, dont_import=False
-    ),
     DataTable("dim_staff", {
         "staff_id": "int",
         "first_name": "varchar",
         "last_name": "varchar",
         "department_name": "varchar",
         "location": "varchar",
-        "email_address": "varchar"}, dont_import=False
-    )
+        "email_address": "varchar"}
+    ),
+    DataTable("dim_location", {
+        "location_id": "int",
+        "address_line_1": "varchar",
+        "address_line_2": "varchar",
+        "district": "varchar",
+        "city": "varchar",
+        "postal_code": "varchar",
+        "country": "varchar",
+        "phone": "varchar"}, dont_import = True
+    ),
+    DataTable("dim_currency", {
+        "currency_id": "INT",
+        "currency_code": "VARCHAR",
+        "currency_name": "VARCHAR"}
+    ),
 ]
