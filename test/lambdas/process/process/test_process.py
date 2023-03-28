2e98cf9f3e355f6faae1f9a78c5f8917b2039655
Diaeddin84 <alarnaouti@gmail.com>
2023-03-28 11:51:59 +0100

    add test_main_s3_outputs_good_parquet

--- test/lambdas/process/process/test_process.py
+++ test/lambdas/process/process/test_process.py
@@ -4,10 +4,49 @@ from pandas import read_parquet
 import pytest
 import os
 import boto3
+import io
+import pandas as pd
 from src.lambdas.process.process import main_s3
 
-processing_bucket_name = "query_queens_processing_bucket"
-ingestion_bucket_name = "query_queens_ingestion_bucket"
+
+
+PROCESSING_BUCKET_NAME = "query_queens_processing_bucket"
+INGESTION_BUCKET_NAME = "query_queens_ingestion_bucket"
+PREFIX = "2020-11-03/14:20:49/"
+
+
+
+
+
+
+class Helpers:
+    @staticmethod
+    def mock_ingestion(s3):
+        s3.create_bucket(Bucket=PROCESSING_BUCKET_NAME)
+        s3.create_bucket(Bucket=INGESTION_BUCKET_NAME)
+        s3.upload_file('./test/lambdas/process/json_files/last_updated.json', INGESTION_BUCKET_NAME, 'date/last_updated.json')
+        s3.upload_file('./test/lambdas/process/json_files/address_test_2.json', INGESTION_BUCKET_NAME, f'{PREFIX}address.json')
+        s3.upload_file('./test/lambdas/process/json_files/counterparty_test_2.json', INGESTION_BUCKET_NAME, f'{PREFIX}counterparty.json')
+        s3.upload_file('./test/lambdas/process/json_files/currency_test_2.json', INGESTION_BUCKET_NAME, f'{PREFIX}currency.json')
+        s3.upload_file('./test/lambdas/process/json_files/department_test_1.json', INGESTION_BUCKET_NAME, f'{PREFIX}department.json')
+        s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{PREFIX}design.json')
+        s3.upload_file('./test/lambdas/process/json_files/staff_test_1.json', INGESTION_BUCKET_NAME, f'{PREFIX}staff.json')
+        s3.upload_file('./test/lambdas/process/json_files/sales_order_test_1.json', INGESTION_BUCKET_NAME, f'{PREFIX}sales_order.json')
+        s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{PREFIX}purchase_order.json')
+        s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{PREFIX}payment.json')
+        s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{PREFIX}payment_type.json')
+        s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{PREFIX}transaction.json')
+        return
+        
+@pytest.fixture
+def helpers():
+    return Helpers
+
+
+
+
+
+
 
 
 @pytest.fixture(scope="module")
@@ -26,21 +65,21 @@ def s3(aws_credentials):
 
 def test_process_main_s3(s3):
     prefix = "2020-11-03/14:20:49/"
-    s3.create_bucket(Bucket=processing_bucket_name)
-    s3.create_bucket(Bucket=ingestion_bucket_name)
-    s3.upload_file('./test/lambdas/process/json_files/last_updated.json', ingestion_bucket_name, 'date/last_updated.json')
-    s3.upload_file('./test/lambdas/process/json_files/address_test_2.json', ingestion_bucket_name, f'{prefix}address.json')
-    s3.upload_file('./test/lambdas/process/json_files/counterparty_test_2.json', ingestion_bucket_name, f'{prefix}counterparty.json')
-    s3.upload_file('./test/lambdas/process/json_files/currency_test_2.json', ingestion_bucket_name, f'{prefix}currency.json')
-    s3.upload_file('./test/lambdas/process/json_files/department_test_1.json', ingestion_bucket_name, f'{prefix}department.json')
-    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', ingestion_bucket_name, f'{prefix}design.json')
-    s3.upload_file('./test/lambdas/process/json_files/staff_test_1.json', ingestion_bucket_name, f'{prefix}staff.json')
-    s3.upload_file('./test/lambdas/process/json_files/sales_order_test_1.json', ingestion_bucket_name, f'{prefix}sales_order.json')
-    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', ingestion_bucket_name, f'{prefix}purchase_order.json')
-    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', ingestion_bucket_name, f'{prefix}payment.json')
-    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', ingestion_bucket_name, f'{prefix}payment_type.json')
-    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', ingestion_bucket_name, f'{prefix}transaction.json')
-    objects = s3.list_objects_v2(Bucket=ingestion_bucket_name)
+    s3.create_bucket(Bucket=PROCESSING_BUCKET_NAME)
+    s3.create_bucket(Bucket=INGESTION_BUCKET_NAME)
+    s3.upload_file('./test/lambdas/process/json_files/last_updated.json', INGESTION_BUCKET_NAME, 'date/last_updated.json')
+    s3.upload_file('./test/lambdas/process/json_files/address_test_2.json', INGESTION_BUCKET_NAME, f'{prefix}address.json')
+    s3.upload_file('./test/lambdas/process/json_files/counterparty_test_2.json', INGESTION_BUCKET_NAME, f'{prefix}counterparty.json')
+    s3.upload_file('./test/lambdas/process/json_files/currency_test_2.json', INGESTION_BUCKET_NAME, f'{prefix}currency.json')
+    s3.upload_file('./test/lambdas/process/json_files/department_test_1.json', INGESTION_BUCKET_NAME, f'{prefix}department.json')
+    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{prefix}design.json')
+    s3.upload_file('./test/lambdas/process/json_files/staff_test_1.json', INGESTION_BUCKET_NAME, f'{prefix}staff.json')
+    s3.upload_file('./test/lambdas/process/json_files/sales_order_test_1.json', INGESTION_BUCKET_NAME, f'{prefix}sales_order.json')
+    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{prefix}purchase_order.json')
+    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{prefix}payment.json')
+    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{prefix}payment_type.json')
+    s3.upload_file('./test/lambdas/process/json_files/design_test_2.json', INGESTION_BUCKET_NAME, f'{prefix}transaction.json')
+    objects = s3.list_objects_v2(Bucket=INGESTION_BUCKET_NAME)
     assert 'Contents' in objects
    
     
@@ -51,6 +90,7 @@ def test_process_main_s3(s3):
     assert len(expected_keys) == len(actual_keys)
     for key in expected_keys:
         assert key in actual_keys
+    
 
     
 
@@ -58,9 +98,30 @@ def test_process_main_s3(s3):
     
 def test_write_to_bucket(s3):
     main_s3()
-    processing_objects = s3.list_objects_v2(Bucket=processing_bucket_name)
+    processing_objects = s3.list_objects_v2(Bucket=PROCESSING_BUCKET_NAME)
     expected_keys = ['2020-11-03/14:20:49/counter_party.parquet', '2020-11-03/14:20:49/currency.parquet', '2020-11-03/14:20:49/date.parquet', '2020-11-03/14:20:49/design.parquet', '2020-11-03/14:20:49/location.parquet', '2020-11-03/14:20:49/sales_order.parquet', '2020-11-03/14:20:49/staff.parquet']
     actual_keys = [d['Key'] for d in processing_objects['Contents']]
     assert len(expected_keys) == len(actual_keys)
     for key in expected_keys:
         assert key in actual_keys
+    
+
+def test_main_s3_outputs_correct_parquet_files(s3, helpers):
+    output_tables = ['counter_party', 'currency', 'date', 'design', 'location', 'sales_order', 'staff']
+    expected_keys = [os.path.join(PREFIX, table + '.parquet') for table in output_tables]
+    helpers.mock_ingestion(s3)
+    main_s3()
+    processed_contents = s3.list_objects_v2(Bucket=PROCESSING_BUCKET_NAME)['Contents']
+    file_names = []
+    for s3_object in processed_contents:
+        s3_filepath = s3_object['Key']
+        obj = s3.get_object(Bucket=PROCESSING_BUCKET_NAME, Key=s3_filepath)
+        df = pd.read_parquet(io.BytesIO(obj['Body'].read()))
+        file_names.append(s3_filepath)
+
+    assert len(expected_keys) == len(file_names)
+    for key in expected_keys:
+        assert key in file_names
+        
+      
+   
