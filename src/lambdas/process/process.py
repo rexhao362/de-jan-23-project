d2bde4378098234fbcf2a4fd1c4b7ec354aab9d3
g-vernon <github@gvernon.com>
2023-03-28 18:44:41 +0100

    refactors process.main_s3

--- src/lambdas/process/process.py
+++ src/lambdas/process/process.py
@@ -125,17 +125,15 @@ def main_local():
     success = False
 
     try:
-        # for table in input_tables where required = true, table[dataframe] = load_file
-
         for key, table in input_tables.items():
             if table['required']:
                 table['dataframe'] = load_file_from_local(
                     join(LOCAL_INGESTION_DIRECTORY, current_timestamp, table['table_name'] + '.json'))
-
         success = True
     except Exception as e:
         # Do something with the exception, log it to Cloudwatch
         logging.error("Couldn't load tables.")
+        success = False
 
     # If all is well, try processing dictionaries
 
@@ -156,147 +154,109 @@ def main_local():
         success = False
         try:
             for table in output_tables:
-                deps = [input_tables[dep]['dataframe'] for dep in table['dependencies'] ]
+                deps = [input_tables[dep]['dataframe']
+                        for dep in table['dependencies']]
                 if (table['table_name'] == 'date'):
-                    #start and end dates generated for the dates table
+                    # start and end dates generated for the dates table
                     deps = ['2020/01/01', '2050/01/01']
                 table['dataframe'] = table['build_function'](*deps)
             success = True
         except Exception as e:
             # Do something with the exception, log it to Cloudwatch
             logging.error("Couldn't remodel dataframes.")
-            print(e)
-            print(traceback.format_exc())
 
     # If all is well, try writing remodeled dataframes to bucket
     if (success):
         try:
-            
+
             for table in output_tables:
-                write_file_to_local(join(LOCAL_PROCESSING_DIRECTORY, current_timestamp), table['dataframe'], table['table_name']+'.parquet')
+                write_file_to_local(join(LOCAL_PROCESSING_DIRECTORY, current_timestamp),
+                                    table['dataframe'], table['table_name']+'.parquet')
             logging.info("All processed tables are written to the bucket.")
 
         except Exception as e:
             # Do something with the exception, tell Cloudwatch, and clean up the bucket
             logging.error("Couldn't write tables to bucket.")
-            print(e)
 
             # TO-DO clean up bucket ticket 85
 
 
 def main_s3():
-    INGESTION_BUCKET_NAME = "query_queens_ingestion_bucket"
-    PROCESSING_BUCKET_NAME = "query_queens_processing_bucket"
+    INGESTION_BUCKET_NAME = "query-queens-ingestion-bucket"
+    PROCESSING_BUCKET_NAME = "query-queens-processing-bucket"
 
     date, time = get_last_updated(INGESTION_BUCKET_NAME)
     jsons = get_all_jsons(INGESTION_BUCKET_NAME, date, time)
 
     current_timestamp = f"{date}/{time}"
 
-    if (len(jsons) == 11):
-        # create a lookup object
-        table_names = {
-            'currency': current_timestamp + '/currency.json',
-            'design': current_timestamp + '/design.json',
-            'staff': current_timestamp + '/staff.json',
-            'department': current_timestamp + '/department.json',
-            # 'purchase_order': current_timestamp + '/purchase_order.json',
-            'counterparty': current_timestamp + '/counterparty.json',
-            'address': current_timestamp + '/address.json',
-            'sales_order': current_timestamp + '/sales_order.json',
-            # 'payment_type': current_timestamp + '/payment_type.json',
-            # 'payment': current_timestamp + '/payment.json',
-            # 'transaction': current_timestamp + '/transaction.json'
-        }
+    for key, table in input_tables.items():
+        table['filename'] = join(
+            current_timestamp, table['table_name'] + '.json')
 
-        # Try loading all the data
+    # Try loading all the data
+    success = False
+
+    try:
+        for key, table in input_tables.items():
+            if table['required']:
+                table['dataframe'] = load_file_from_s3(INGESTION_BUCKET_NAME, join(
+                    current_timestamp, table['table_name'] + '.json'))
+        success = True
+    except Exception as e:
+        # Do something with the exception, log it to Cloudwatch
+        logging.error("Couldn't load tables.")
         success = False
 
+    # If all is well, try processing dictionaries
+
+    if (success):
+        success = False
         try:
-            dim_currency = load_file_from_s3(
-                INGESTION_BUCKET_NAME, table_names['currency'])
-            dim_design = load_file_from_s3(
-                INGESTION_BUCKET_NAME, table_names['design'])
-            dim_staff = load_file_from_s3(
-                INGESTION_BUCKET_NAME, table_names['staff'])
-            dim_counterparty = load_file_from_s3(
-                INGESTION_BUCKET_NAME, table_names['counterparty'])
-            fact_sales_order = load_file_from_s3(
-                INGESTION_BUCKET_NAME, table_names['sales_order'])
-            dim_department = load_file_from_s3(
-                INGESTION_BUCKET_NAME, table_names['department'])
-            dim_address = load_file_from_s3(
-                INGESTION_BUCKET_NAME, table_names['address'])
+            for key, table in input_tables.items():
+                if table['required']:
+                    table['dataframe'] = process(table['dataframe'])
             success = True
         except Exception as e:
             # Do something with the exception, log it to Cloudwatch
-            logging.error("Couldn't load tables.")
-
-        # If all is well, try processing dictionaries
-
-        if (success):
+            logging.error("Couldn't process tables.")
             success = False
-            try:
-                dim_currency = process(dim_currency)
-                dim_design = process(dim_design)
-                dim_staff = process(dim_staff)
-                dim_location = process(dim_address)
-                dim_counterparty = process(dim_counterparty)
-                fact_sales_order = process(fact_sales_order)
-                dim_department = process(dim_department)
-                dim_address = process(dim_address)
-                success = True
-            except Exception as e:
-                # Do something with the exception, log it to Cloudwatch
-                print(e)
-                logging.error("Couldn't process tables.")
 
-        # if all is well, try remodeling dataframes
+    # if all is well, try remodeling dataframes
 
-        if (success):
+    if (success):
+        success = False
+        try:
+            for table in output_tables:
+                deps = [input_tables[dep]['dataframe']
+                        for dep in table['dependencies']]
+                if (table['table_name'] == 'date'):
+                    # start and end dates generated for the dates table
+                    deps = ['2020/01/01', '2050/01/01']
+                table['dataframe'] = table['build_function'](*deps)
+            success = True
+        except Exception as e:
+            # Do something with the exception, log it to Cloudwatch
+            logging.error("Couldn't remodel dataframes.")
             success = False
-            try:
-                dim_currency = build_dim_currency(dim_currency)
-                dim_design = build_dim_design(dim_design)
-                dim_staff = build_dim_staff(dim_staff, dim_department)
-                dim_location = build_dim_location(dim_address)
-                dim_date = build_dim_date("2020/01/01", "2050/01/01")
-                dim_counterparty = build_dim_counterparty(
-                    dim_counterparty, dim_address)
-                fact_sales_order = build_fact_sales_order(fact_sales_order)
-                success = True
-            except Exception as e:
-                # Do something with the exception, log it to Cloudwatch
-                print(e)
-                logging.error("Couldn't remodel dataframes.")
 
-        # If all is well, try writing remodeled dataframes to bucket
-        if (success):
-            try:
-                write_to_bucket(PROCESSING_BUCKET_NAME, dim_currency,
-                                current_timestamp + "/currency")
-                write_to_bucket(PROCESSING_BUCKET_NAME, dim_design,
-                                current_timestamp + "/design")
-                write_to_bucket(PROCESSING_BUCKET_NAME, dim_staff,
-                                current_timestamp + "/staff")
-                write_to_bucket(PROCESSING_BUCKET_NAME, dim_location,
-                                current_timestamp + "/location")
-                write_to_bucket(PROCESSING_BUCKET_NAME, dim_date,
-                                current_timestamp + "/date")
-                write_to_bucket(PROCESSING_BUCKET_NAME, dim_counterparty,
-                                current_timestamp + "/counterparty")
-                write_to_bucket(PROCESSING_BUCKET_NAME, fact_sales_order,
-                                current_timestamp + "/sales_order")
+    # If all is well, try writing remodeled dataframes to bucket
+    if (success):
+        try:
+            for table in output_tables:
+                write_to_bucket(PROCESSING_BUCKET_NAME, table['dataframe'], join(
+                    current_timestamp, table['table_name']+'.parquet'))
 
-                logging.info("All processed tables are written to the bucket.")
+            logging.info("All processed tables are written to the bucket.")
 
-            except Exception as e:
-                # Do something with the exception, tell Cloudwatch, and clean up the bucket
-                logging.error("Couldn't write tables to bucket.")
+        except Exception as e:
+            # Do something with the exception, tell Cloudwatch, and clean up the bucket
+            logging.error("Couldn't write tables to bucket.")
+            success = False
 
-                # TO-DO clean up bucket ticket 85
+            # TO-DO clean up bucket ticket 85
 
 
 if __name__ == "__main__":
-    # main_s3()
-    main_local()
+    main_s3()
+    # main_local()
