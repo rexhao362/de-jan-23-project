875c4b057f0ed796d3fedf7e7445d3ca6a24aebc
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-29 12:13:59 +0100

    change schema to reflect NOT NULL conditions

--- src/lambdas/load/db_schema.py
+++ src/lambdas/load/db_schema.py
@@ -2,69 +2,69 @@ from src.lambdas.load.utils.data_table import DataTable
 
 db_schema = [
     DataTable("dim_staff", {
-        "staff_id": "INT",
-        "first_name": "VARCHAR",
-        "last_name": "VARCHAR",
-        "department_name": "VARCHAR",
-        "location": "VARCHAR",
-        "email_address": "VARCHAR" }, dont_import = False
+        "staff_id": "INT NOT NULL",
+        "first_name": "VARCHAR NOT NULL",
+        "last_name": "VARCHAR NOT NULL",
+        "department_name": "VARCHAR NOT NULL",
+        "location": "VARCHAR NOT NULL",
+        "email_address": "VARCHAR NOT NULL" }, dont_import = True
     ),
     DataTable("dim_location", {
-        "location_id": "INT",
-        "address_line_1": "VARCHAR",
+        "location_id": "INT NOT NULL",
+        "address_line_1": "VARCHAR NOT NULL",
         "address_line_2": "VARCHAR",
         "district": "VARCHAR",
-        "city": "VARCHAR",
-        "postal_code": "VARCHAR",
-        "country": "VARCHAR",
-        "phone": "VARCHAR" }, dont_import = True
+        "city": "VARCHAR NOT NULL",
+        "postal_code": "VARCHAR NOT NULL",
+        "country": "VARCHAR NOT NULL",
+        "phone": "VARCHAR NOT NULL" }, dont_import = False
     ),
     DataTable("dim_currency", {
-        "currency_id": "INT",
-        "currency_code": "VARCHAR",
-        "currency_name": "VARCHAR" }, dont_import = False
+        "currency_id": "INT NOT NULL",
+        "currency_code": "VARCHAR NOT NULL",
+        "currency_name": "VARCHAR NOT NULL" }, dont_import = True
     ),
     DataTable("dim_design", {
-        "design_id": "INT",
-        "design_name": "VARCHAR",
-        "file_location": "VARCHAR",
-        "file_name": "VARCHAR" }, dont_import = False
+        "design_id": "INT NOT NULL",
+        "design_name": "VARCHAR NOT NULL",
+        "file_location": "VARCHAR NOT NULL",
+        "file_name": "VARCHAR NOT NULL" }, dont_import = True
     ),
     DataTable("dim_date", {
-        "date_id": "DATE",
-        "year": "INT",
-        "month": "INT",
-        "day": "INT",
-        "day_of_week": "INT",
-        "day_name": "VARCHAR",
-        "month_name": "VARCHAR",
-        "quarter": "INT" }, dont_import = True
+        "date_id": "DATE NOT NULL",
+        "year": "INT NOT NULL",
+        "month": "INT NOT NULL",
+        "day": "INT NOT NULL",
+        "day_of_week": "INT NOT NULL",
+        "day_name": "VARCHAR NOT NULL",
+        "month_name": "VARCHAR NOT NULL",
+        "quarter": "INT NOT NULL" }, dont_import = True
     ),
     DataTable("dim_counterparty", {
-        "counterparty_id": "INT",
-        "counterparty_legal_name": "VARCHAR",
-        "counterparty_legal_address_line_1": "VARCHAR",
+        "counterparty_id": "INT NOT NULL",
+        "counterparty_legal_name": "VARCHAR NOT NULL",
+        "counterparty_legal_address_line_1": "VARCHAR NOT NULL",
         "counterparty_legal_address_line_2": "VARCHAR",
         "counterparty_legal_district": "VARCHAR",
-        "counterparty_legal_city": "VARCHAR",
-        "counterparty_legal_postal_code": "VARCHAR",
-        "counterparty_legal_country": "VARCHAR",
-        "counterparty_legal_phone_number": "VARCHAR" }, dont_import = False
+        "counterparty_legal_city": "VARCHAR NOT NULL",
+        "counterparty_legal_postal_code": "VARCHAR NOT NULL",
+        "counterparty_legal_country": "VARCHAR NOT NULL",
+        "counterparty_legal_phone_number": "VARCHAR NOT NULL" }, dont_import = True
     ),
     DataTable("fact_sales_order", {
-        "sales_order_id": "INT",
-        "created_date": "DATE",
-        "created_time": "TIME",
-        "last_updated_date": "DATE",
-        "last_updated_time": "TIME",
-        "sales_staff_id": "INT",
-        "counterparty_id": "INT",
-        "units_sold": "INT",
-        "unit_price": "NUMERIC(10, 2)",
-        "currency_id": "INT",
-        "design_id": "INT",
-        "agreed_payment_date": "DATE",
-        "agreed_delivery_date": "DATE",
-        "agreed_delivery_location_id": "INT" }, dont_import = True
+        "sales_order_id": "INT NOT NULL",
+        "created_date": "DATE NOT NULL",
+        "created_time": "TIME NOT NULL",
+        "last_updated_date": "DATE NOT NULL",
+        "last_updated_time": "TIME NOT NULL",
+        "sales_staff_id": "INT NOT NULL",
+        "counterparty_id": "INT NOT NULL",
+        "units_sold": "INT NOT NULL",
+        "unit_price": "NUMERIC(10, 2) NOT NULL",
+        "currency_id": "INT NOT NULL",
+        "design_id": "INT NOT NULL",
+        "agreed_payment_date": "DATE NOT NULL",
+        "agreed_delivery_date": "DATE NOT NULL",
+        "agreed_delivery_location_id": "INT NOT NULL" }, dont_import = True
     )
 ]
