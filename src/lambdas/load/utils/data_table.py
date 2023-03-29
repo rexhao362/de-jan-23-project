4a55094509ee3310b32d8bb879add59e4aab3112
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-29 11:24:14 +0100

    add __format function to handle null values and escape single quotes

--- src/lambdas/load/utils/data_table.py
+++ src/lambdas/load/utils/data_table.py
@@ -1,4 +1,4 @@
-import os
+from os.path import join
 import pyarrow.parquet as pq
 from src.lambdas.load.utils.data_table_source import \
     DataFromPyArrowTable, DataFromParquetFile
@@ -33,19 +33,23 @@ class DataTable:
         file_name = override_file_name if override_file_name \
             else f'{self.name}{default_parquet_extension}'
 
-        full_path = os.path.join(path, file_name)
+        full_path = join(path, file_name)
         table = pq.read_table(full_path)
         return self.__from_source(table, DataFromParquetFile(full_path) )
 
     def __from_source(self, table, source):                                                                                                                                                                                                                                                                                                                                                                                                     
         self.table = table.select(self.column_names)
-        self.__check_column_types()
+        #self.__check_column_types()
         self.source = source
         return self
 
     @staticmethod
-    def __escape(element):
-        return str(element).replace("'", r"''")
+    def __format(element):
+        """
+        Returns a "NULL" string if was passed None to get the correct value in DB
+        Escapes single quotes in values by adding a single quote (psql style) and encloses the result in single quotes
+        """
+        return "NULL" if element == None else "'" + str(element).replace("'", r"''") + "'"
 
     def to_sql_values(self):
         assert self.has_data()
@@ -53,7 +57,7 @@ class DataTable:
         rows = self.table.to_pylist()
         rows_strs = []
         for row_number, row in enumerate(rows):
-            row_str = "(" + ', '.join( [ f"'{self.__escape(element)}'" for element in row.values() ] ) + ")"
+            row_str = "(" + ', '.join( [ f'{self.__format(element)}' for element in row.values() ] ) + ")"
             rows_strs.append(row_str)
 
         return ',\n'.join(rows_strs)
