eab5df7811702bb39a9c46b126640a0b130b4534
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-03-29 13:36:34 +0100

    import all tables

--- src/lambdas/load/db_schema.py
+++ src/lambdas/load/db_schema.py
@@ -7,7 +7,7 @@ db_schema = [
         "last_name": "VARCHAR NOT NULL",
         "department_name": "VARCHAR NOT NULL",
         "location": "VARCHAR NOT NULL",
-        "email_address": "VARCHAR NOT NULL" }, dont_import = True
+        "email_address": "VARCHAR NOT NULL" }, dont_import = False
     ),
     DataTable("dim_location", {
         "location_id": "INT NOT NULL",
@@ -22,13 +22,13 @@ db_schema = [
     DataTable("dim_currency", {
         "currency_id": "INT NOT NULL",
         "currency_code": "VARCHAR NOT NULL",
-        "currency_name": "VARCHAR NOT NULL" }, dont_import = True
+        "currency_name": "VARCHAR NOT NULL" }, dont_import = False
     ),
     DataTable("dim_design", {
         "design_id": "INT NOT NULL",
         "design_name": "VARCHAR NOT NULL",
         "file_location": "VARCHAR NOT NULL",
-        "file_name": "VARCHAR NOT NULL" }, dont_import = True
+        "file_name": "VARCHAR NOT NULL" }, dont_import = False
     ),
     DataTable("dim_date", {
         "date_id": "DATE NOT NULL",
@@ -38,7 +38,7 @@ db_schema = [
         "day_of_week": "INT NOT NULL",
         "day_name": "VARCHAR NOT NULL",
         "month_name": "VARCHAR NOT NULL",
-        "quarter": "INT NOT NULL" }, dont_import = True
+        "quarter": "INT NOT NULL" }, dont_import = False
     ),
     DataTable("dim_counterparty", {
         "counterparty_id": "INT NOT NULL",
@@ -49,7 +49,7 @@ db_schema = [
         "counterparty_legal_city": "VARCHAR NOT NULL",
         "counterparty_legal_postal_code": "VARCHAR NOT NULL",
         "counterparty_legal_country": "VARCHAR NOT NULL",
-        "counterparty_legal_phone_number": "VARCHAR NOT NULL" }, dont_import = True
+        "counterparty_legal_phone_number": "VARCHAR NOT NULL" }, dont_import = False
     ),
     DataTable("fact_sales_order", {
         "sales_order_id": "INT NOT NULL",
@@ -65,6 +65,6 @@ db_schema = [
         "design_id": "INT NOT NULL",
         "agreed_payment_date": "DATE NOT NULL",
         "agreed_delivery_date": "DATE NOT NULL",
-        "agreed_delivery_location_id": "INT NOT NULL" }, dont_import = True
+        "agreed_delivery_location_id": "INT NOT NULL" }, dont_import = False
     )
 ]
