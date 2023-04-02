f58b45035b8f15ac49fec55bb093d0ba3a8c9ea6
Dmitry Skorokhod <dmitry.skorokhod@gmail.com>
2023-04-02 21:52:21 +0100

    add S3 upload trigger

---
+++ deployment/terraform/notification.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket_notification" "process_bucket" {
+  bucket = aws_s3_bucket.process_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.load_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    #filter_prefix       = "foldername"
+    filter_suffix = ".txt"
+  }
+
+  depends_on = [aws_lambda_permission.s3_permission_to_trigger_load_lambda]
+}
+
+resource "aws_lambda_permission" "s3_permission_to_trigger_load_lambda" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.load_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.process_bucket.arn
+}
